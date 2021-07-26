b1808750fc3ecf359fcdda94f6080e808f04bc10
unknown <coleyang2016@gmail.com>
2021-07-26 14:03:10 -0700

    fix header bug

--- js/common.js
+++ js/common.js
@@ -1,7 +1,30 @@
 /* fixed header add transition animation */
 
-var header = document.getElementById("header");
+/* 
+    parament:
+        1. obj  object name
+        2. ev   event name
+        3. fn   return function
+*/ 
+function addEvent(obj,ev,fn){
+    /* normal browser */
+    if(obj.addEventListener){
+        obj.addEventListener(ev,fn);
+    }
+    /* IE browser */
+    else{
+        obj.attachEvent("on" + ev, fn);
+    }
 
-if(window.pageYOffset > header.offsetTop){
-    header.className += " fixed";
 }
+
+
+function fixNav(){
+    var header = document.getElementById("header");
+
+    if(window.pageYOffset > header.offsetTop){
+        header.className += " fixed";
+    }
+}
+
+addEvent(window,"scroll",fixNav);
